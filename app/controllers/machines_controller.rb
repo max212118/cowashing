fcb66cfa64c7e7abe94869ad2bac2812fefcec47
maxence tarnaud <maxence.trnd@gmail.com>
2022-02-22 13:34:10 +0100

    create views

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,19 +1,32 @@
 class MachinesController < ApplicationController
+
   def index
+    @machines = Machine.all
   end
 
   def show
+    @machine = Machine.find(params[:id])
   end
 
   def new
+    @machine = Machine.new
   end
 
   def create
+    @machine =Machine.new(machine_params)
+    @machine.save
+    redirect_to machines_path
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+  
+  def machine_params
+    params.require(:machine).permit(:brand, :capacity, :location, :price)
   end
 end
