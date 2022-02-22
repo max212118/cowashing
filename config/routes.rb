9ad159027df07a16efeb2a48a13ee062db15d1fd
Aris Gyalui <arisdgyalui@outlook.com>
2022-02-22 17:31:25 +0100

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :appointements, only: [:new, :create, :index]
+
   resources :appointements do
     resources :reviews, only: [:new, :create]
   end
