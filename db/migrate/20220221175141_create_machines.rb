941deea6a61fd88bc2970fcf3e2130213d67a2ee
maxence tarnaud <maxence.trnd@gmail.com>
2022-02-22 11:16:29 +0100

    migration of model machine

--- db/migrate/20220221175141_create_machines.rb
+++ db/migrate/20220221175141_create_machines.rb
@@ -5,6 +5,7 @@ class CreateMachines < ActiveRecord::Migration[6.1]
       t.integer :capacity
       t.string :location
       t.integer :price
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
