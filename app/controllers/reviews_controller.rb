ded2acabf62c17e7e633dc37a90f2a3bab37f1d8
Aris Gyalui <arisdgyalui@outlook.com>
2022-02-22 12:40:44 +0100

    updated the review controller with three methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(params[:review])
+    @review.save
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  redirect_to reviews_path
 end
