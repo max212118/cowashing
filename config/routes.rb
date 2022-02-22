5556a900c78d2aaf40322fc2fd28580663628788
maxence tarnaud <maxence.trnd@gmail.com>
2022-02-22 13:08:15 +0100

    machines controller views routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'machines/index'
-  get 'machines/show'
-  get 'machines/new'
-  get 'machines/create'
-  get 'machines/edit'
-  get 'machines/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :machines
 end
